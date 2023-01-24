d2b5d0ed95d7f9582e81a4045c84fd1487c336d3
LexHeadko <n-odes11rus@yandex.ru>
2023-01-24 19:40:02 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
-        //task3();
+        task2();
+        task3();
         //task4();
         //task5();
         //task6();
@@ -13,8 +13,16 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        for (int i =1; i<=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
 }
