3cc2886145efd0f4e3cb36da6e9883f1541c7277
LexHeadko <n-odes11rus@yandex.ru>
2023-01-25 11:39:53 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();
+        //task3();
         //task4();
         //task5();
         //task6();
